9f1eef064b1bcb5fc6c3fb876b217755e8d5d5c1
susanto-tm <37474291+susanto-tm@users.noreply.github.com>
2018-08-26 17:07:33 +0700

    Css-icons testing

--- WebApp/static/WebApp/js/default.js
+++ WebApp/static/WebApp/js/default.js
@@ -1,5 +1,10 @@
 var main = function(){
     // Insert functions here
+    $('.nav-icon').click(function(){
+        $('.icon')
+            .toggleClass('menu')
+            .toggleClass('close');
+    })
 };
 
 
