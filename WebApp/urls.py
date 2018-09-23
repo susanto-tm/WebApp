2978dc9bd019f0bb9cb06bbb9324191732ac10f7
susanto-tm <37474291+susanto-tm@users.noreply.github.com>
2018-09-23 15:33:47 +0700

    Fixed admin link in urls.py

--- WebApp/urls.py
+++ WebApp/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
 ]
 urlpatterns += staticfiles_urlpatterns()
