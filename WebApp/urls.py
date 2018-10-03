19fd256448938eea301fa0eed6ede82de50da139
susanto-tm <37474291+susanto-tm@users.noreply.github.com>
2018-10-03 20:18:21 +0700

    added links to the home page

--- WebApp/urls.py
+++ WebApp/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from WebApp import views
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
