ad9d4421c3fa8b5d2f62666f7b13a68a01e716e1
susanto-tm <37474291+susanto-tm@users.noreply.github.com>
2018-12-14 10:15:46 +0700

    replaced forum-post.html and fixed other styling issues

--- WebApp/static/WebApp/js/default.js
+++ WebApp/static/WebApp/js/default.js
@@ -5,6 +5,14 @@ var main = function(){
             .toggleClass('menu')
             .toggleClass('close'); 
     })
+    $('.content-markdown').each(function() {
+        var content = $(this).text();
+        console.log(content);
+        var markedContent = marked(content);
+        console.log(markedContent);
+        $(this).html(markedContent);
+
+    })
 };
 
 
